b4d92a83e00967a4c67954960b310bc29521ed61
lopezmateo <163901238+lopezmateo@users.noreply.github.com>
2024-04-08 14:19:08 -0300

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n1,n2,n3,m;
+	cout<<"ingrese el primer numero"<<endl;
+	cin>>n1;
+	cout<<"ingrese el segundo numero"<<endl;
+	cin>>n2;
+	cout<<"ingrese el tercer numero"<<endl;
+	cin>>n3;
+	
+	m= n1+n2+n3;
+	m=m/3;
+	cout<<"el promedio es: "<<m<<endl;
+	
+	
+return 0;	
+	
+}
